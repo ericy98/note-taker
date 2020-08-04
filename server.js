86f0d6ecf879cc6ffe605e30a768866790f6ba89
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-08-04 17:00:42 -0500

    set up main server file

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
+// parse string/array data
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+// parse JSON data
+app.use(express.json());
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// middleware instruct server to make public folder avaliable (HTML/CSS/JS)
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
 });
