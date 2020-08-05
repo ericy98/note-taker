254516a13d0c7cbe9aaf13fbaa9c4c1c7a2e9bec
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-08-05 12:44:07 -0500

    change storage

--- db/store.js
+++ db/store.js
@@ -1,43 +1,39 @@
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 
-function read() {
-    return fs.readFile("db/db.json", "utf8");
-};
-
-function write(note) {
-    return fs.writeFile("db/db.json", JSON.stringify(note));
-};
-
-function getNotes() {
-    return this.read()
-        .then(notes => {
-            const parsedNotes = [].concat(JSON.stringify(notes));
-            return parsedNotes;
-        });
-
-};
-
-function addNotes() {
-    const { title, text } = note;
-
-    if (!title || !text) {
-        throw new Error("'Title' and 'Text' cannot be blank. Please try again.");
+class Storage{
+    read() {
+        return fs.readFile("db/db.json", "utf8");
     }
 
-    const newNote = { title, text, id: uuidv4() };
+    write(note) {
+        return fs.writeFile("db/db.json", JSON.stringify(note));
+    }
 
-    return this.getNotes() 
-        .then(notes => {
-            [...notes, newNote]
-        })
-        .then(updatedNotes => this.write(updatedNotes))
-        .then(() => { newNote });
-};
-
-console.log(read, write);
-
-module.exports = {
-    getNotes, 
-    addNotes
+    getNotes() {
+        return this.read()
+            .then(notes => {
+                const parsedNotes = [].concat(JSON.parse(notes));
+                return parsedNotes;
+            });
+    
+    }
+    addNotes() {
+        const { title, text } = note;
+    
+        if (!title || !text) {
+            throw new Error("'Title' and 'Text' cannot be blank. Please try again.");
+        }
+    
+        const newNote = { title, text, id: uuidv4() };
+    
+        return this.getNotes() 
+            .then(notes => {
+                [...notes, newNote]
+            })
+            .then(updatedNotes => this.write(updatedNotes))
+            .then(() => { newNote });
+    }
 }
+
+module.exports = new Storage();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,18 @@
 const router = require('express').Router();
-const {getNotes, addNotes} = require('../db/store');
+const Storage = require('../db/store');
 
 router.get('/notes', (req, res) => {
-    getNotes()
-    .then(notes => res.json(notes))
-    .catch(err => res.status(500).json(err));
+    Storage
+        .getNotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err));
 });
 
 router.post ('/notes', (req, res) => {
-    addNotes()
-    .then((note) => res.json(note))
-    .catch(err => res.status(500).json(err));
+    Storage
+        .addNotes()
+        .then((note) => res.json(note))
+        .catch(err => res.status(500).json(err));
 });
 
 module.exports = router;
